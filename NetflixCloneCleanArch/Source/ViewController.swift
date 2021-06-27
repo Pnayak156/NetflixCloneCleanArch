7652d4cf149647b9829e962241a3abdac268415a
Prashanta Kumar Nayak <prashanta.nayak@myntra.com>
2021-06-27 17:14:17 +0530

    setup swiftlint

--- NetflixCloneCleanArch/Source/ViewController.swift
+++ NetflixCloneCleanArch/Source/ViewController.swift
@@ -8,12 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var name: String?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
